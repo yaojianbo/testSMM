d826ecd42e15c72c024d8e094af6b5549ec8b89a
yaojianbo <bobomiao1988@163.com>
2016-08-03 17:49:37 +0800

    测试拦截器

--- src/main/java/com/bobo/testSSM/service/impl/UserServiceImpl.java
+++ src/main/java/com/bobo/testSSM/service/impl/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package com.bobo.testSSM.service.impl;
 
-import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bobo.testSSM.dao.IUserDao;
@@ -11,12 +11,13 @@ import com.bobo.testSSM.service.IUserService;
 @Service("userService")
 public class UserServiceImpl implements IUserService 
 {
-	@Resource
+	@Autowired
 	private IUserDao userDao;
 
 	@Override
 	public User getUserById(int userID) 
 	{
+		// System.out.println("getUserById 执行");
 		return this.userDao.selectByPrimaryKey(userID);
 	}
 
