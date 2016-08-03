192a1e14ab1827a0f2b9bb68ac440912a75e7245
yaojianbo <bobomiao1988@163.com>
2016-08-03 17:48:39 +0800

    使用JSON作为请求参数post

---
+++ src/main/java/com/bobo/testSSM/controller/request/GetUserRequest.java
@@ -0,0 +1,14 @@
+package com.bobo.testSSM.controller.request;
+
+public class GetUserRequest 
+{
+	private int userID;
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+}
